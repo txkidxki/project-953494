de335f49b107acd3cbe6210c6d39b30e9293ffac
txkidxki <rattanakorn_re@cmu.ac.th>
2022-01-29 15:10:04 +0700

    edit main page

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   onPressed: () {},
                 ),
                 Container(
-                    width: 125.0,
+                    width: 130.0,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
+                        // IconButton(
+                        //   icon: Icon(Icons.filter_list),
+                        //   color: Colors.white,
+                        //   onPressed: () {},
+                        // ),
                         IconButton(
-                          icon: Icon(Icons.filter_list),
-                          color: Colors.white,
-                          onPressed: () {},
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.menu),
+                          icon: Icon(Icons.search_rounded),
                           color: Colors.white,
                           onPressed: () {},
                         )
@@ -61,14 +61,14 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: EdgeInsets.only(left: 40.0),
             child: Row(
               children: <Widget>[
-                Text('Healthy',
+                Text('Food',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 25.0)),
                 SizedBox(width: 10.0),
-                Text('Food',
+                Text('Menu',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         color: Colors.white,
@@ -92,59 +92,59 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Container(
                         height: MediaQuery.of(context).size.height - 300.0,
                         child: ListView(children: [
-                          _buildFoodItem('assets/plate1.png', 'Salmon bowl', '\$24.00'),
-                          _buildFoodItem('assets/plate2.png', 'Spring bowl', '\$22.00'),
-                          _buildFoodItem('assets/plate6.png', 'Avocado bowl', '\$26.00'),
-                          _buildFoodItem('assets/plate5.png', 'Berry bowl', '\$24.00')
+                          _buildFoodItem('assets/plate1.png', 'Salmon bowl', ''),
+                          _buildFoodItem('assets/plate2.png', 'Spring bowl', ''),
+                          _buildFoodItem('assets/plate6.png', 'Avocado bowl', ''),
+                          _buildFoodItem('assets/plate5.png', 'Berry bowl', '')
                         ]))),
                     Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.search, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.shopping_basket, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 120.0,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.grey,
-                              style: BorderStyle.solid,
-                              width: 1.0),
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Color(0xFF1C1428)),
-                      child: Center(
-                          child: Text('Checkout',
-                              style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  color: Colors.white,
-                                  fontSize: 15.0))),
-                    )
+                    // Container(
+                    //   height: 65.0,
+                    //   width: 60.0,
+                    //   decoration: BoxDecoration(
+                    //     border: Border.all(
+                    //         color: Colors.grey,
+                    //         style: BorderStyle.solid,
+                    //         width: 1.0),
+                    //     borderRadius: BorderRadius.circular(10.0),
+                    //   ),
+                    //   child: Center(
+                    //     child: Icon(Icons.search, color: Colors.black),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   height: 65.0,
+                    //   width: 60.0,
+                    //   decoration: BoxDecoration(
+                    //     border: Border.all(
+                    //         color: Colors.grey,
+                    //         style: BorderStyle.solid,
+                    //         width: 1.0),
+                    //     borderRadius: BorderRadius.circular(10.0),
+                    //   ),
+                    //   child: Center(
+                    //     child: Icon(Icons.shopping_basket, color: Colors.black),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   height: 65.0,
+                    //   width: 120.0,
+                    //   decoration: BoxDecoration(
+                    //       border: Border.all(
+                    //           color: Colors.grey,
+                    //           style: BorderStyle.solid,
+                    //           width: 1.0),
+                    //       borderRadius: BorderRadius.circular(10.0),
+                    //       color: Color(0xFF1C1428)),
+                    //   child: Center(
+                    //       child: Text('Checkout',
+                    //           style: TextStyle(
+                    //               fontFamily: 'Montserrat',
+                    //               color: Colors.white,
+                    //               fontSize: 15.0))),
+                    // )
                   ],
                 )
               ],
@@ -205,7 +205,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 )
               ),
               IconButton(
-                icon: Icon(Icons.add),
+                icon: Icon(Icons.star_border),
                 color: Colors.black,
                 onPressed: () {}
               )
